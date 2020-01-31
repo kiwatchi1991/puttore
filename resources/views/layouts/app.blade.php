cabf0ef42002ee0bf9dbe5b758385ffb9d607eea
脇田　慎平 <ymnkknt3@gmail.com>
2020-01-31 13:42:23 +0900

    機能実装

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -70,7 +70,7 @@
                                         {{ __('Logout') }}
                                     </a>
 
-                                    <form id="" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                                         @csrf
                                     </form>
                                 </div>
