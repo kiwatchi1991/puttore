ec49382970ec7a66ea07fe09d8706f76b156edf4
脇田　慎平 <ymnkknt3@gmail.com>
2020-02-05 08:16:26 +0900

    コンフリ解消

--- app/Product.php
+++ app/Product.php
@@ -6,11 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-<<<<<<< HEAD
-    protected $fillable = ['name', 'detail', 'lesson'];
-=======
     protected $fillable = ['name', 'detail', 'lesson', 'default_price'];
->>>>>>> refs/heads/feature/ユーザー登録設定各種
     
     //多対多のリレーションを作る
     public function categories()
