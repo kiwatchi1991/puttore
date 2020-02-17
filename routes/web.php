87651c91ba5baa5d27ff2be256741dfaf228d1e4
脇田　慎平 <ymnkknt3@gmail.com>
2020-02-17 09:31:22 +0900

    profile関係ルーティング

--- routes/web.php
+++ routes/web.php
@@ -23,16 +23,19 @@ Route::group(['middleware' => 'check'], function () {
     Route::get('/products/new', 'ProductsController@new')->name('products.new');
     Route::post('/products/new', 'ProductsController@create')->name('products.create');
     Route::get('/products',  'ProductsController@index')->name('products');
+    Route::post('/products',  'ProductsController@index')->name('products');
+    Route::get('/products/{id}',  'ProductsController@show')->name('products.show');
     Route::get('/products/{id}/edit', 'ProductsController@edit')->name('products.edit');
     Route::post('/products/{id}/edit', 'ProductsController@update')->name('products.update');
     Route::post('/products/{id}/delete', 'ProductsController@delete')->name('products.delete');
     Route::get('/products/mypage', 'ProductsController@mypage')->name('products.mypage');
-    Route::post('/products/mypage', 'ProductsController@mypage')->name('products.search');
+    // Route::post('/products/mypage', 'ProductsController@mypage')->name('products.search');
     Route::post('/logout', 'Auth\LoginController@logout')->name('logout');
 
-    //プロフィール編集
-    Route::get('/profile/{id}/edit', 'Auth\ProfileController@edit')->name('edit');
-    Route::post('/profile/{id}/edit', 'Auth\ProfileController@update')->name('update');
+    //プロフィール
+    Route::get('/profile/{id}/edit', 'ProfilesController@edit')->name('profile.edit');
+    Route::post('/profile/{id}/edit', 'ProfilesController@update')->name('profile.update');
+    Route::get('/profile/{id}',  'ProfilesController@show')->name('profile.show');
 
 
 });
