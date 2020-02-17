d9dcd24bffc6cde68be310c0ab89d20cf8ad70fa
脇田　慎平 <ymnkknt3@gmail.com>
2020-02-17 09:30:17 +0900

    bladeファイル（プロフィール編集）新規作成

---
+++ resources/views/profile/edit.blade.php
@@ -0,0 +1,104 @@
+@extends('layouts.app')
+
+@section('content')
+
+<section class="register">
+    <div class="l-profile__inner">
+    <div class="c-pageTitle">プロフィールを編集する</div>
+
+        <form method="POST" action="{{ route('profile.update', $user->id) }}" enctype="multipart/form-data">
+            @csrf
+
+            {{-- 画像 --}}
+            <div class="c-input__area">
+                <label for="pic"
+                    >プロフィール画像</label>
+
+                    <div class="c-image__preview">
+                        <p class="c-delete__file">消す</p>
+                        <label class="c-area__drop">
+                            <input class="c-input__file" type="file" name="pic" >
+                        <img src="/storage/{{ $user->pic }}" alt=""  class="c-prev__img">
+                        ドラッグ＆ドロップ
+                        </label>
+                    </div>
+
+                    @error('pic')
+                    <span role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                    @enderror
+                </div>
+            {{-- アカウント名 --}}
+            <div class="c-input__area">
+                <label for="account_name"
+                    >アカウント名</label>
+
+                <div>
+                    <input id="account_name" type="text"
+                    class="c-input__form @error('account_id') is-invalid @enderror" name="account_name"
+                        required autocomplete="account_name" value="{{ $user->account_name }}">
+
+                    @error('account_name')
+                    <span role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                    @enderror
+                </div>
+
+            {{-- 肩書き --}}
+            <div class="c-input__area">
+                <label for="account_title"
+                    >肩書き</label>
+
+                <div>
+                    <input id="account_title" type="text"
+                    class="c-input__form @error('account_id') is-invalid @enderror" name="account_title"
+                        required autocomplete="account_title" value="{{ $user->account_title }}">
+
+                    @error('account_title')
+                    <span role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                    @enderror
+                </div>
+            </div>
+
+            {{-- 自己紹介 --}}
+            <div class="c-input__area">
+                <label for="account_detail"
+                    >自己紹介</label>
+
+                <div>
+                    <input id="account_detail" type="text"
+                    class="c-input__form @error('account_id') is-invalid @enderror" name="account_detail"
+                        required autocomplete="account_detail" value="{{ $user->account_detail }}">
+
+                    @error('account_detail')
+                    <span role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                    @enderror
+                </div>
+            </div>
+
+            {{-- 保存ボタン --}}
+            <div class="c-button__block">
+                <button type="submit" class="c-button">
+                   登録する
+                </button>
+            </div>
+        </form>
+
+    </div>
+</section>
+
+            
+
+ユーザーID
+{{ $user->id }}
+ああ
+
+
+
+@endsection
