1c00e20e1a47e555b048d0df050272d3df941823
脇田　慎平 <ymnkknt3@gmail.com>
2020-02-17 09:27:16 +0900

    ユーザーモデル

--- app/User.php
+++ app/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'pic','account_name','account_title','account_detail', 'email', 'password',
     ];
 
     /**
