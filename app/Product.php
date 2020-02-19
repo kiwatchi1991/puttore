954ec50d3dab92a2f17fa0ad7731bd43c8bcf5e9
脇田　慎平 <ymnkknt3@gmail.com>
2020-02-19 13:40:18 +0900

    モデル追記

--- app/Product.php
+++ app/Product.php
@@ -9,15 +9,23 @@ class Product extends Model
     protected $fillable = ['name', 'detail', 'lesson', 'default_price', 'pic1' ];
     
     //多対多のリレーションを作る
+    //カテゴリー
     public function categories()
     {
         return $this->belongsToMany('App\Category');
     }
 
+    //難易度
     public function difficulties()
     {
         return $this->belongsToMany('App\Difficulty');
     }
+
+     // 1対多のリレーションを作る
+     public function lessons()
+     {
+         return $this->hasMany('App\Lesson');
+     }
 
     // 検索機能
     public function scopeTagFilter($query, ?string $tag)
