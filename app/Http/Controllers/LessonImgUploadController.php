e8dad0e8e53646fcfc840f34990306fdfe8b0d03
脇田　慎平 <ymnkknt3@gmail.com>
2020-03-06 09:37:18 +0900

    コントローラー記述追加中

--- app/Http/Controllers/LessonImgUploadController.php
+++ app/Http/Controllers/LessonImgUploadController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Lessonimg;
 use Illuminate\Support\Facades\Log;
 
 class LessonImgUploadController extends Controller
@@ -16,6 +17,11 @@ class LessonImgUploadController extends Controller
         Log::debug('<<<<<<<< imgupload ajax発動！>>>>>>>>>>>>>');
         Log::debug('<<<<<<<< $request 内容 >>>>>>>>>>>>>');
         Log::debug($request);
+
+        //画像登録
+        $Lessonimg = new Lessonimg;
+        $Lessonimg->fill($request->all())->save();
+        $isPic = $request->pic1;
 
         return response()->json('OK');
     }
