24950e9693f52e8124dbcdb95fc7858925922c9d
脇田　慎平 <ymnkknt3@gmail.com>
2020-03-15 05:29:56 +0900

    ぱすわーどリセット

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,47 +1,28 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
+<div class="c-passReset">
+    <h2 class="c-passReset__title">パスワードをリセットする</h2>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+    <form method="POST" action="{{ route('password.email') }}">
+        @csrf
 
-                    <form method="POST" action="{{ route('password.email') }}">
-                        @csrf
+        <div class="c-passReset__prev-area c-passReset__prev-area__mail">
+            <label for="email" class="c-passReset__label">メールアドレス</label>
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Send Password Reset Link') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
+            <div>
+                <input class="c-passReset__input" name="email"
+                    value="{{ old('email') }} @error('email') is-invalid @enderror" required type="email">
             </div>
+            @error('email')
+            <span class="c-passReset__error" role="alert">
+                <strong>{{ $message }}</strong>
+            </span>
+            @enderror
         </div>
-    </div>
+        <button type="submit" class="c-passReset__submit">
+            パスワードリセットリンクを送信する
+        </button>
+    </form>
 </div>
 @endsection
