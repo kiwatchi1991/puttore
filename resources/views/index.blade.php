12c4c53ec43eb86691edb669d9dc7c498e19b402
脇田　慎平 <ymnkknt3@gmail.com>
2020-03-15 20:51:13 +0900

    TOPデザイン完成

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,88 +2,98 @@
 
 @section('title','TOP')
 @section('content')
-<div class="c-top__fv">
-    <div class="c-top__fvImg">
-        <img src="" alt="">FVイラスト
+<div class="c-top__container">
+    <div class="c-top__fv">
+        <div class="c-top__fvImg">
+
+            <img class="c-top__fvImg__img" src="/storage/images/fv-image.png" alt="" width="185" height="120">
+        </div>
+        <div class="c-top__fvMsg">
+            <h2 class="c-top__fvMsg__head">プログラミング<br>学習中のあなたへ</h2>
+            <p class="c-top__fvMsg__text">「どのくらいのレベルで作ればいいの？」<br>「アウトプットしたいけど、何を作ればいいの？」</p>
+
+            <h3 class="c-top__fvMsg c-top__fvMsg__subject">テキスト型アウトプット教材サービス</h3>
+            <h1 class="c-top__fvMsg c-top__fvMsg__title">『ぷっとれ』</h1>
+
+            <div class="c-top__button">
+                <a class="c-top__button--link" href="{{ route('products') }}">教材を見てみる</a>
+            </div>
+
+        </div>
     </div>
-    <div class="c-top__fvMsg">
-        <h2 class="c-top__text">プログラミング<br>学習中のあなたへ</h2>
-        <p class="c-top__text c-top__text--fvMsg">「どのくらいのレベルで作ればいいの？」</p>
-        <p class="c-top__text c-top__text--fvMsg">「アウトプットしたいけど、何を作ればいいの？」</p>
+    <div class="c-top__output">
+        <h2 class="c-top__output__head">あなたも作れるようになる！</h2>
+        <p class="c-top__output__text__sample">-アウトプット例-</p>
 
-        <h3 class="c-top__text c-top__text--subject">テキスト型アウトプット教材サービス</h3>
-        <h1 class="c-top__text c-top__text--title">『ぷっとれ』</h1>
+        <div class="c-top__output__Imgs">
+            <div class="c-top__output__Img"><img src="/storage/images/top-4.png" alt="">イラスト</div>
+            <div class="c-top__output__Img c-top__output__Img--left"><img src="/storage/images/top-5.png" alt="">イラスト
+            </div>
+            <div class="c-top__output__Img"><img src="/storage/images/top-4.png" alt="">イラスト</div>
+        </div>
+    </div>
+    <div class="c-top__recommends">
+        <h2 class="c-top__recommends__head">こんな人にオススメ</h2>
 
+        <div class="c-top__recommend">
+
+            <div class="c-top__recommend__Img">
+                <h3 class="c-top__recommend__head">何を作ればいいの？</h3>
+                <img src="/storage/images/top-1.png" alt="">
+                <p class="c-top__recommend__text">たくさんの教材を用意していますので、<br>もうアウトプットのネタに<br>困ることはありません！</p>
+            </div>
+        </div>
+        <div class="c-top__recommend">
+            <div class="c-top__recommend__Img c-top__recommend__Img--how">
+                <h3 class="c-top__recommend__head">どうやって作ればいいの？</h3>
+                <img src="/storage/images/top-2.png" alt="">
+                <p class="c-top__recommend__text">教材に沿って進めていくだけで<br>実務レベルのアウトプットが完成します。</p>
+            </div>
+        </div>
+        <div class="c-top__recommend">
+            <div class="c-top__recommend__Img">
+                <h3 class="c-top__recommend__head">初心者でも大丈夫？</h3>
+                <img src="/storage/images/top-3.png" alt="">
+                <p class="c-top__recommend__text">実際のコードや画像を使ってわかりやすく<br>説明しているので、初心者でも安心。</p>
+            </div>
+        </div>
+    </div>
+
+    <div id="features" class="c-top__features">
+        <div class="c-top__features__inner">
+            <div class="c-top__features__subject">
+                <p>これまでにない、<br>テキスト型のアウトプット教材<br>専門のサービスです</p>
+            </div>
+            {{-- <div class="c-top__features__title"> --}}
+            <h2 class="c-top__features__title">「ぷっとれ」の特徴</h2>
+            {{-- </div> --}}
+            <div class="c-top__feature">
+                <h3 class="c-top__feature__title">初心者OK</h3>
+                <p class="c-top__feature__text">コードだけでなく画像を交えて説明して<br>あるので、初心者でも安心して進められ<br>ます！</p>
+            </div>
+            <div class="c-top__feature">
+                <h3 class="c-top__feature__title">現場主義</h3>
+                <p class="c-top__feature__text">簡単なものを作るだけでは意味がありま<br>せん。実際の現場で使える知識が身につ<br>く質の高い教材をご用意！</p>
+            </div>
+            <div class="c-top__feature">
+                <h3 class="c-top__feature__title">本格的なデザイン</h3>
+                <p class="c-top__feature__text">そのままポートフォリオとしても使える<br>見た目にもこだわった教材で、<br>周囲と差をつけよう！</p>
+            </div>
+            <div class="c-top__feature">
+                <h3 class="c-top__feature__title">レベル・言語ごとに豊富な教材</h3>
+                <p class="c-top__feature__text">スキルレベル・言語ごとに豊富な教材を<br>用意していますので、あなたにぴったり<br>のものがきっと見つかるはずです！</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="c-top__end">
+        <div class="c-top__end__subject">
+            プログラミングは、<br>アウトプットしなければ<br>絶対に上達しません！
+        </div>
+        <h2 class="c-top__end__title">さあ、アウトプットしよう！</h2>
         <div class="c-top__button">
             <a class="c-top__button--link" href="{{ route('products') }}">教材を見てみる</a>
         </div>
-
-    </div>
-</div>
-<div class="c-top__outputSample">
-    <h2>あなたも作れるようになる！</h2>
-    <p>-アウトプット例-</p>
-
-    <div class="c-top__outputImgs">
-        <div class="c-top__outputImg"><img src="" alt="">イラスト</div>
-        <div class="c-top__outputImg"><img src="" alt="">イラスト</div>
-        <div class="c-top__outputImg"><img src="" alt="">イラスト</div>
-    </div>
-</div>
-<div class="c-top__recommends">
-    <h2>こんな人にオススメ</h2>
-
-    <div class="c-top__recommend">
-        <h3>何を作ればいいの？</h3>
-        <div class="c-top__recommendImg"><img src="" alt="">イラスト</div>
-        <p class="c-top__text c-top__text--recommend">たくさんの教材を用意していますので、<br>もうアウトプットのネタに<br>困ることはありません！</p>
-    </div>
-    <div class="c-top__recommend">
-        <h3>どうやって作ればいいの？</h3>
-        <div class="c-top__recommendImg"><img src="" alt="">イラスト</div>
-        <p class="c-top__text c-top__text--recommend">教材に沿って進めていくだけで<br>実務レベルのアウトプットが完成します。</p>
-    </div>
-    <div class="c-top__recommend">
-        <h3>初心者でも大丈夫？</h3>
-        <div class="c-top__recommendImg"><img src="" alt="">イラスト</div>
-        <p class="c-top__text c-top__text--recommend">実際のコードや画像を使ってわかりやすく<br>説明しているので、初心者でも安心</p>
-    </div>
-</div>
-<div class="c-top__features">
-    <div class="c-top__features__subject">
-        <p>これまでにない、<br>テキスト型のアウトプット教材<br>専門のサービスです</p>
-    </div>
-    <div class="c-top__features__title">
-
-        <h2>『ぷっとれ』の特徴</h2>
-    </div>
-    <div class="c-top__feature">
-        <h3 class="c-top__feature__title">初心者OK</h3>
-        <p class="c-top__text c-top__text--feature">コードだけでなく画像を交えて<br>説明してるので、初心者でも安心して<br>進められます！</p>
-    </div>
-    <div class="c-top__feature">
-        <h3 class="c-top__feature__title">現場主義</h3>
-        <p class="c-top__text c-top__text--feature">簡単なものを作るだけでは意味がありません。<br>実際の現場で使える知識が身に付く<br>質の高い教材をご用意！</p>
-    </div>
-    <div class="c-top__feature">
-        <h3 class="c-top__feature__title">本格的なデザイン</h3>
-        <p class="c-top__text c-top__text--feature">そのままポートフォリオとしても使える<br>見た目にもこだわった教材で、周囲と差をつけよう！</p>
-    </div>
-    <div class="c-top__feature">
-        <h3 class="c-top__feature__title">レベル・言語ごとに豊富な教材</h3>
-        <p class="c-top__text c-top__text--feature">スキルレベル・言語ごとに豊富な教材を用意<br>していますので、あなたにぴったりのものが<br>きっと見つかるはずです！</p>
-    </div>
-</div>
-
-<div class="c-top__last">
-    <div class="c-top__last__subject">
-        プログラミングは、<br>アウトプットしなければ<br>絶対に上達しません！
-    </div>
-    <div class="c-top__last__title">
-        <h2>さあ、アウトプットしよう！</h2>
-    </div>
-    <div class="c-top__button">
-        <a class="c-top__button--link" href="{{ route('products') }}">教材を見てみる</a>
     </div>
 </div>
 @section('content')
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -36,115 +36,119 @@
 </head>
 
 <body>
-    <div class="container">
-        <nav class="header">
-            <div class="header__inner">
-                <div class="header__logo">
-                    <a class="" href="{{ url('/') }}">
-                        <img src="/storage/images/logo.png" alt="ぷっとれ" width="120">
-                    </a>
-                </div>
-
-                <div class="" id="navbarSupportedContent">
-                    <!-- Right Side Of Navbar -->
-                    <ul class="">
-                        <div>
-
-                            <nav class="global-nav">
-                                <ul class="global-nav__list">
-
-                                    @auth
-
-                                    <li class="global-nav__item global-nav__item--profile">
-                                        <a href="{{ route('profile.show',$user->id) }}"
-                                            class="global-nav__item__link lobal-nav__item__link--profile">
-                                            <div class="global-nav__item__profile">
-                                                <div class="global-nav__item__profile__img__wrapper">
-                                                    @if($user->pic)
-                                                    <img class="global-nav__item__profile__img"
-                                                        {{-- src="{{ /storage/ $user->pic  }}" alt=""> --}}
-                                                    src="/storage/{{ $user->pic }}" alt="">
-                                                    @endif
-                                                </div>
-                                                <span
-                                                    class="global-nav__item__profile__name">{{ $user->account_name }}</span>
-                                            </div>
-                                        </a></li>
-                                    @endauth
-
-                                    <li class="global-nav__item">
-                                        <a href="{{ route('products') }}"
-                                            class="global-nav__item__link @guest first @endguest">作品を見る</a>
-                                    </li>
-                                    <li class="global-nav__item">
-                                        <a href="{{ route('products.new') }}" class="global-nav__item__link">出品する</a>
-                                    </li>
-                                    @auth
-                                    <li class="global-nav__item">
-                                        <a href=" {{ route('profile.show',$user->id) }}"
-                                            class="global-nav__item__link">マイページ</a>
-                                    </li>
-                                    <li class="global-nav__item">
-                                        <a href=" {{ route('bords') }}" class="global-nav__item__link">メッセージボード</a>
-                                    </li>
-                                    <li class="global-nav__item">
-                                        <a onclick="event.preventDefault();
-                                             document.getElementById('logout-form').submit();"
-                                            href="{{ route('logout') }}" class="global-nav__item__link">ログアウト</a>
-                                        {{-- <form action="/logout" method="POST" id="logout__form" style="display:none;"></form> --}}
-                                        <form id="logout-form" action="{{ route('logout') }}" method="POST"
-                                            style="display: none;">
-                                            @csrf
-                                        </form>
-                                    </li>
-                                    @endauth
-                                    @guest
-                                    <li class="global-nav__item">
-                                        <a href=" {{ route('login') }}" class="global-nav__item__link auth">ログイン</a>
-                                        <a href=" {{ route('register') }}" class="global-nav__item__link auth">新規登録</a>
-                                    </li>
-                                    @endguest
-                                </ul>
-                            </nav>
-                            <div class="hamburger" id="js-hamburger">
-                                <span class="hamburger__line hamburger__line--1"></span>
-                                <span class="hamburger__line hamburger__line--2"></span>
-                                <span class="hamburger__line hamburger__line--3"></span>
-                            </div>
-                            <div class="black-bg" id="js-black-bg">
-                            </div>
-                        </div>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-
-        {{-- フラッシュメッセージ --}}
-        @if (session('flash_message'))
-        <div class="c-header__sessionMessage js-sessionMessage" style="display: none;">
-            {{ session('flash_message') }}
-        </div>
-        @endif
-
-        <main class="py-4">
-            @yield('content')
-        </main>
-        <footer>
+    {{-- <div class="container"> --}}
+    <nav class="header">
+        <div class="header__inner">
             <div class="header__logo">
                 <a class="" href="{{ url('/') }}">
                     <img src="/storage/images/logo.png" alt="ぷっとれ" width="120">
                 </a>
             </div>
-            <div>
-                <ul>
-                    <li><a href="">ぷっとれについて</a></li>
-                    <li><a href="">利用規約</a></li>
-                    <li><a href="">プライバシーポリシー</a></li>
-                    <li><a href="{{ route('contact.index') }}">お問い合わせ</a></li>
+
+            <div class="" id="navbarSupportedContent">
+                <!-- Right Side Of Navbar -->
+                <ul class="">
+                    <div>
+
+                        <nav class="global-nav">
+                            <div class="cover"></div>
+                            <ul class="global-nav__list">
+
+                                @auth
+
+                                <li class="global-nav__item global-nav__item--profile">
+                                    <a href="{{ route('profile.show',$user->id) }}"
+                                        class="global-nav__item__link lobal-nav__item__link--profile">
+                                        <div class="global-nav__item__profile">
+                                            <div class="global-nav__item__profile__img__wrapper">
+                                                @if($user->pic)
+                                                <img class="global-nav__item__profile__img"
+                                                    {{-- src="{{ /storage/ $user->pic  }}" alt=""> --}}
+                                                src="/storage/{{ $user->pic }}" alt="">
+                                                @endif
+                                            </div>
+                                            <span
+                                                class="global-nav__item__profile__name">{{ $user->account_name }}</span>
+                                        </div>
+                                    </a></li>
+                                @endauth
+
+                                <li class="global-nav__item">
+                                    <a href="{{ route('products') }}"
+                                        class="global-nav__item__link @guest first @endguest">作品を見る</a>
+                                </li>
+                                <li class="global-nav__item">
+                                    <a href="{{ route('products.new') }}" class="global-nav__item__link">出品する</a>
+                                </li>
+                                @auth
+                                <li class="global-nav__item">
+                                    <a href=" {{ route('profile.show',$user->id) }}"
+                                        class="global-nav__item__link">マイページ</a>
+                                </li>
+                                <li class="global-nav__item">
+                                    <a href=" {{ route('bords') }}" class="global-nav__item__link">メッセージボード</a>
+                                </li>
+                                <li class="global-nav__item">
+                                    <a onclick="event.preventDefault();
+                                             document.getElementById('logout-form').submit();"
+                                        href="{{ route('logout') }}" class="global-nav__item__link">ログアウト</a>
+                                    {{-- <form action="/logout" method="POST" id="logout__form" style="display:none;"></form> --}}
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST"
+                                        style="display: none;">
+                                        @csrf
+                                    </form>
+                                </li>
+                                @endauth
+                                @guest
+                                <li class="global-nav__item">
+                                    <a href=" {{ route('login') }}" class="global-nav__item__link auth">ログイン</a>
+                                    <a href=" {{ route('register') }}" class="global-nav__item__link auth">新規登録</a>
+                                </li>
+                                @endguest
+                            </ul>
+                        </nav>
+                        <div class="hamburger" id="js-hamburger">
+                            <span class="hamburger__line hamburger__line--1"></span>
+                            <span class="hamburger__line hamburger__line--2"></span>
+                            <span class="hamburger__line hamburger__line--3"></span>
+                        </div>
+                        <div class="black-bg" id="js-black-bg">
+                        </div>
+                    </div>
                 </ul>
             </div>
-        </footer>
+        </div>
+    </nav>
+
+    {{-- フラッシュメッセージ --}}
+    @if (session('flash_message'))
+    <div class="c-header__sessionMessage js-sessionMessage" style="display: none;">
+        {{ session('flash_message') }}
     </div>
+    @endif
+
+    <main class="py-4">
+        @yield('content')
+    </main>
+    <footer id="footer">
+        <div class="c-footer__inner">
+            <div class="c-footer__logo">
+                <a class="" href="{{ url('/') }}">
+                    <img src="/storage/images/logo.png" alt="ぷっとれ" width="120">
+                </a>
+            </div>
+            <div class="c-footer__list">
+                <ul>
+                    <li><a class="c-footer__list__link" href="/#features">ぷっとれについて</a></li>
+                    <li><a class="c-footer__list__link" href="">利用規約</a></li>
+                    <li><a class="c-footer__list__link" href="">プライバシーポリシー</a></li>
+                    <li><a class="c-footer__list__link" href="{{ route('contact.index') }}">お問い合わせ</a></li>
+                </ul>
+                <p class="c-footer__small"><small>©︎2020 ぷっとれ</small></p>
+            </div>
+        </div>
+    </footer>
+    {{-- </div> --}}
     <script src="{{ asset('js/app.js') }}"></script>
     {{-- <script type="text/javascript" src="{{ asset('slick/slick.min.js') }}"></script> --}}
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
