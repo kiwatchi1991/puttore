a245a71d8dd84b263dcf77f4fc4f5b3934661018
脇田　慎平 <ymnkknt3@gmail.com>
2020-03-19 01:37:36 +0900

    メールテキスト体裁

--- resources/views/mail/html/passwordreset.blade.php
+++ resources/views/mail/html/passwordreset.blade.php
@@ -1,37 +1,37 @@
 <!DOCTYPE html>
 <html lang="ja">
-<style>
-    body {
-        background-color: #fffacd;
-    }
 
-    h1 {
-        font-size: 16px;
-        color: #ff6666;
-    }
-
-    #button {
-        width: 200px;
-        text-align: center;
-    }
-
-    #button a {
-        padding: 10px 20px;
-        display: block;
-        border: 1px solid #2a88bd;
-        background-color: #FFFFFF;
-        color: #2a88bd;
-        text-decoration: none;
-        box-shadow: 2px 2px 3px #f5deb3;
-    }
-
-    #button a:hover {
-        background-color: #2a88bd;
-        color: #FFFFFF;
-    }
-</style>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="">
+    <title>Document</title>
+</head>
 
 <body>
+
+    <style>
+        #button {
+            width: 200px;
+            text-align: center;
+        }
+
+        #button a {
+            padding: 10px 20px;
+            display: block;
+            background-color: #7DD4BB;
+            border-radius: 10px;
+            color: white;
+            text-decoration: none;
+        }
+
+        #button a:hover {
+            background-color: #2a88bd;
+            color: #FFFFFF;
+        }
+    </style>
+
     <h1>
         パスワードリセット
     </h1>
@@ -41,6 +41,7 @@
     <p id="button">
         <a href="{{$reset_url}}">パスワードリセット</a>
     </p>
+
 </body>
 
 </html>
