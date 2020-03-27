90232c738dced360cecb019216cca51290f83220
脇田　慎平 <ymnkknt3@gmail.com>
2020-03-27 22:25:59 +0900

    退会ページ　スマホ見た目

--- resources/views/profile/delete.blade.php
+++ resources/views/profile/delete.blade.php
@@ -1,13 +1,22 @@
 @extends('layouts.app')
 @section('title','退会')
 @section('content')
-退会しますか？
 
-ユーザーid : {{ $user->id }}
-<form id="form" method="POST" action="{{ route('profile.deleteData',$user->id) }}" enctype="multipart/form-data">
-    @csrf
-    <input type="hidden" name="id" value="{{ $user->id }}" />
-    <button type="submit">はい</button>
-</form>
+<div class="c-profileDelete">
+    <h2 class="c-profileDelete__title">退会しますか？</h2>
 
+    <div class="c-profileDelete__form">
+
+        <form id="form" method="POST" action="{{ route('profile.deleteData',$user->id) }}"
+            enctype="multipart/form-data">
+            @csrf
+            <input type="hidden" name="id" value="{{ $user->id }}" />
+            <div class="c-profileDelete__submit">
+
+                <button class="c-profileDelete__submit__btn" type="submit">はい</button>
+            </div>
+        </form>
+    </div>
+
+</div>
 @endsection
