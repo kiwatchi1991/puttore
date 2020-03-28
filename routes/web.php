e4b6a5c55030ae11dd755da317d160c226dd5869
脇田　慎平 <ymnkknt3@gmail.com>
2020-03-28 14:36:53 +0900

    ルーティング修正？

--- routes/web.php
+++ routes/web.php
@@ -61,7 +61,6 @@ Route::group(['middleware' => ['auth', 'can:admin']], function () {
   Route::get('/admin/transfer/update/{id}',  'adminController@transferUpdateConfirm')->name('admin.transfer.update.confirm');
   Route::post('/admin/transfer/update/{id}',  'adminController@transferUpdateConfirm')->name('admin.transfer.update.confirm');
   Route::post('/admin/transfer/update/{id?}',  'adminController@transferUpdate')->name('admin.transfer.update');
-
 });
 
 //===========================================================================
@@ -70,7 +69,7 @@ Route::group(['middleware' => ['auth', 'can:admin']], function () {
 Auth::routes();
 
 Route::get('/', function () {
-    return view('index');
+  return view('index');
 });
 Route::get('/',  'indexController@index')->name('home');
 
@@ -90,63 +89,63 @@ Route::get('/contacts/finish', 'ContactController@finish')->name('contact.finish
 //ログインユーザーのみ
 //===========================================================================
 Route::group(['middleware' => 'check'], function () {
-    Route::get('/products/new', 'ProductsController@new')->name('products.new');
-    Route::post('/products/new', 'ProductsController@create')->name('products.create');
-    Route::get('/products/{id}/edit', 'ProductsController@edit')->name('products.edit');
-    Route::post('/products/{id}/edit', 'ProductsController@update')->name('products.update');
-    Route::post('/products/{id}/delete', 'ProductsController@delete')->name('products.delete');
-    Route::post('/products/ajaxlike',  'LikesController@ajaxlike')->name('products.ajaxlike');
-    Route::post('/products/ajaxfollow',  'FollowsController@ajaxfollow')->name('products.ajaxfollow');
-    //レッスンの画像アップロード
-    Route::post('/products/imgupload',  'LessonImgUploadController@imgupload')->name('products.imgupload');
+  Route::get('/products/new', 'ProductsController@new')->name('products.new');
+  Route::post('/products/new', 'ProductsController@create')->name('products.create');
+  Route::get('/products/{id}/edit', 'ProductsController@edit')->name('products.edit');
+  Route::post('/products/{id}/edit', 'ProductsController@update')->name('products.update');
+  Route::post('/products/{id}/delete', 'ProductsController@delete')->name('products.delete');
+  Route::post('/products/ajaxlike',  'LikesController@ajaxlike')->name('products.ajaxlike');
+  Route::post('/products/ajaxfollow',  'FollowsController@ajaxfollow')->name('products.ajaxfollow');
+  //レッスンの画像アップロード
+  Route::post('/products/imgupload',  'LessonImgUploadController@imgupload')->name('products.imgupload');
 
-    //レッスン編集画面で削除ボタンを押したときに、DBに既にあるレッスンだった場合はDBから削除
-    Route::post('/products/ajaxLessonDelete',  'ProductsController@ajaxLessonDelete')->name('products.ajaxLessonDelete');
+  //レッスン編集画面で削除ボタンを押したときに、DBに既にあるレッスンだった場合はDBから削除
+  Route::post('/products/ajaxLessonDelete',  'ProductsController@ajaxLessonDelete')->name('products.ajaxLessonDelete');
 
-    //レッスン詳細
-    Route::get('/products/{p_id}/{l_id}',  'LessonShowController@index')->name('lessons');
-
-
-    //カート
-    Route::get('/carts',  'CartsController@index')->name('carts');
-    Route::post('/carts',  'CartsController@ajaxcart')->name('ajaxcarts');
-
-    //マイページ
-    Route::get('/mypage',  'mypageController@index')->name('mypage');
-    Route::get('/mypage/draft',  'mypageController@draft')->name('mypage.draft');
-    Route::get('/mypage/like',  'mypageController@like')->name('mypage.like');
-    Route::get('/mypage/buy',  'mypageController@buy')->name('mypage.buy');
-    Route::get('/mypage/sale',  'mypageController@sale')->name('mypage.sale');
-    Route::get('/mypage/products',  'mypageController@products')->name('mypage.products');
-    Route::get('/mypage/order',  'mypageController@order')->name('mypage.order');
-    Route::get('/mypage/order/{month}',  'mypageController@orderMonth')->name('mypage.order.show');
-    Route::get('/mypage/transfer',  'mypageController@requestTransfer')->name('mypage.order.transfer');
-    Route::get('/mypage/paid',  'mypageController@paid')->name('mypage.order.paid');
-
-    //注文・トークルーム
-    Route::post('/products/{id}',  'OrdersController@create')->name('orders.create');
-    Route::get('/bords',  'BordsController@index')->name('bords');
-    Route::get('/bords/{id}',  'BordsController@show')->name('bords.show');
-    Route::post('/bords',  'MessagesController@create')->name('messages.create');
+  //レッスン詳細
+  Route::get('/products/{p_id}/{l_id}',  'LessonShowController@index')->name('lessons');
 
 
+  //カート
+  Route::get('/carts',  'CartsController@index')->name('carts');
+  Route::post('/carts',  'CartsController@ajaxcart')->name('ajaxcarts');
 
-    //ユーザー
-    Route::get('/profile/{id}/edit', 'ProfilesController@edit')->name('profile.edit');
-    Route::post('/profile/{id}/edit', 'ProfilesController@update')->name('profile.update');
-    Route::get('/profile/{id}',  'ProfilesController@show')->name('profile.show');
-    Route::get('/profile/{id}/delete',  'ProfilesController@deleteShow')->name('profile.deleteShow');
-    Route::post('/profile/{id}/delete',  'ProfilesController@deleteData')->name('profile.deleteData');
-    Route::post('/logout', 'Auth\LoginController@logout')->name('logout');
+  //マイページ
+  Route::get('/mypage',  'mypageController@index')->name('mypage');
+  Route::get('/mypage/draft',  'mypageController@draft')->name('mypage.draft');
+  Route::get('/mypage/like',  'mypageController@like')->name('mypage.like');
+  Route::get('/mypage/buy',  'mypageController@buy')->name('mypage.buy');
+  Route::get('/mypage/sale',  'mypageController@sale')->name('mypage.sale');
+  Route::get('/mypage/products',  'mypageController@products')->name('mypage.products');
+  Route::get('/mypage/order',  'mypageController@order')->name('mypage.order');
+  Route::get('/mypage/order/{month}',  'mypageController@orderMonth')->name('mypage.order.show');
+  Route::get('/mypage/transfer',  'mypageController@requestTransfer')->name('mypage.order.transfer');
+  Route::get('/mypage/paid',  'mypageController@paid')->name('mypage.order.paid');
 
-    //パスワード変更
-    Route::get('changepassword', 'HomeController@showChangePasswordForm');
-    Route::post('changepassword', 'HomeController@changePassword')->name('changepassword');
+  //注文・トークルーム
+  Route::post('/products/{id}',  'OrdersController@create')->name('orders.create');
+  Route::get('/bords',  'BordsController@index')->name('bords');
+  Route::get('/bords/{id}',  'BordsController@show')->name('bords.show');
+  Route::post('/bords',  'MessagesController@create')->name('messages.create');
 
-    // メールアドレス確認メールを送信
-    Route::get('/changeEmail', 'ChangeEmailController@index');
-    Route::get('/reset/{token}', 'ChangeEmailController@reset');
-    Route::post('/email', 'ChangeEmailController@sendChangeEmailLink');
+
+
+  //ユーザー
+  Route::get('/profile/{id}/edit', 'ProfilesController@edit')->name('profile.edit');
+  Route::post('/profile/{id}/edit', 'ProfilesController@update')->name('profile.update');
+  Route::get('/profile/{id}',  'ProfilesController@show')->name('profile.show');
+  Route::get('/profile/{id}/delete',  'ProfilesController@deleteShow')->name('profile.deleteShow');
+  Route::post('/profile/{id}/delete',  'ProfilesController@deleteData')->name('profile.deleteData');
+  Route::post('/logout', 'Auth\LoginController@logout')->name('logout');
+
+  //パスワード変更
+  Route::get('changepassword', 'HomeController@showChangePasswordForm');
+  Route::post('changepassword', 'HomeController@changePassword')->name('changepassword');
+
+  // メールアドレス確認メールを送信
+  Route::get('/changeEmail', 'ChangeEmailController@index');
+  Route::get('/reset/{token}', 'ChangeEmailController@reset');
+  Route::post('/email', 'ChangeEmailController@sendChangeEmailLink');
 });
 
 Route::get('/products',  'ProductsController@index')->name('products');
