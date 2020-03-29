b694a75b7bdff5b1955ea6512db50adbc3a0fd6a
脇田　慎平 <ymnkknt3@gmail.com>
2020-03-29 23:56:17 +0900

    不要なconsole削除

--- public/js/app.js
+++ public/js/app.js
@@ -39794,10 +39794,8 @@ $('.js-request-transfer').on('click', function (e) {
   var confirm_result = window.confirm('振込依頼をします。よろしいですか？');
 
   if (confirm_result) {
-    console.log('OK');
     location.href = '/mypage/transfer';
-  } else {
-    console.log('ダメ');
+  } else {//
   }
 });
 
--- resources/js/components/request-transfer.js
+++ resources/js/components/request-transfer.js
@@ -4,10 +4,9 @@ $('.js-request-transfer').on('click',
     e.preventDefault();
     let confirm_result = window.confirm('振込依頼をします。よろしいですか？');
     if (confirm_result) {
-        console.log('OK');
         location.href = '/mypage/transfer';
     } else {
-        console.log('ダメ');
+        //
     }
 }
 
