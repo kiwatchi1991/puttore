0881a116a1b7c6991ad82e38cf3d61c446a11ede
脇田　慎平 <ymnkknt3@gmail.com>
2020-04-03 17:00:47 +0900

    下書き時もバリデーション効かせる

--- app/Http/Requests/CreateProductRequest.php
+++ app/Http/Requests/CreateProductRequest.php
@@ -28,29 +28,24 @@ class CreateProductRequest extends FormRequest
         Log::debug('<<<<  product バリデーションちぇっく！ >>>>>>>');
         Log::debug($request);
 
-        if ($request->postType !== 'draft') {
 
-            return [
-                'name' => 'required|string|max:255',
-                'lang' => 'required',
-                'difficult' => 'required',
-                'detail' => 'required|string|max:255',
-                'skills' => 'required|string|max:255',
-                'default_price' => 'required|integer|min:100',
+        return [
+            'name' => 'required|string|max:255',
+            'lang' => 'required',
+            'difficult' => 'required',
+            'detail' => 'required|string|max:255',
+            'skills' => 'required|string|max:255',
+            'default_price' => 'required|integer|min:100',
 
-                'lessons.*.title' => 'required|max:255',
-                'lessons.*.lesson' => 'required|',
+            'lessons.*.title' => 'required|max:255',
+            'lessons.*.lesson' => 'required|',
 
-                'pic1' => 'nullable|image',
-                'pic2' => 'nullable|image',
-                'pic3' => 'nullable|image',
-                'pic4' => 'nullable|image',
-                'pic5' => 'nullable|image',
-                'pic6' => 'nullable|image',
-            ];
-        } else {
-            Log::debug('<<<<<<   下書きだからバリデーションしない  >>>>>');
-            return [];
-        }
+            'pic1' => 'nullable|image',
+            'pic2' => 'nullable|image',
+            'pic3' => 'nullable|image',
+            'pic4' => 'nullable|image',
+            'pic5' => 'nullable|image',
+            'pic6' => 'nullable|image',
+        ];
     }
 }
