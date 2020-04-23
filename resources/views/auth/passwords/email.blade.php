ecfbfa435c69d92bf65ca03b6c51e34c335f73d0
脇田　慎平 <ymnkknt3@gmail.com>
2020-04-23 18:31:35 +0900

    パスワードリセット画面　見た目

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,28 +1,31 @@
 @extends('layouts.app')
+@section('title','パスワードリセット')
 
 @section('content')
 <div class="c-passReset">
-    <h2 class="c-passReset__title">パスワードをリセットする</h2>
+    <div class="c-passReset__inner">
+        <h2 class="c-passReset__title">パスワードをリセットする</h2>
 
-    <form method="POST" action="{{ route('password.email') }}">
-        @csrf
+        <form method="POST" action="{{ route('password.email') }}">
+            @csrf
 
-        <div class="c-passReset__prev-area c-passReset__prev-area__mail">
-            <label for="email" class="c-passReset__label">メールアドレス</label>
+            <div class="c-passReset__prev-area c-passReset__prev-area__mail">
+                <label for="email" class="c-passReset__label">メールアドレス</label>
 
-            <div>
-                <input class="c-passReset__input @error('email') is-invalid @enderror" name="email"
-                    value="{{ old('email') }}" required type="email">
+                <div>
+                    <input class="c-passReset__input @error('email') is-invalid @enderror" name="email"
+                        value="{{ old('email') }}" required type="email">
+                </div>
+                @error('email')
+                <span class="c-passReset__error" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+                @enderror
             </div>
-            @error('email')
-            <span class="c-passReset__error" role="alert">
-                <strong>{{ $message }}</strong>
-            </span>
-            @enderror
-        </div>
-        <button type="submit" class="c-passReset__submit">
-            パスワードリセットリンクを送信する
-        </button>
-    </form>
+            <button type="submit" class="c-passReset__submit">
+                パスワードリセット<br class="sp">リンクを送信する
+            </button>
+        </form>
+    </div>
 </div>
 @endsection
--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -1,4 +1,5 @@
 @extends('layouts.app')
+@section('title','パスワードリセット')
 
 @section('content')
 <div class="c-passReset__inner">
