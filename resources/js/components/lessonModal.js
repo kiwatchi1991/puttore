99a495730341c6b5d45eadf223656e1844b89431
脇田　慎平 <ymnkknt3@gmail.com>
2020-05-01 16:16:50 +0900

    コンテンツ登録の画像削除　jsのみ

--- resources/js/components/lessonModal.js
+++ resources/js/components/lessonModal.js
@@ -8,3 +8,53 @@ $(function () {
         return false;
     });
 });
+
+//画像のモーダル表示〜削除まで
+$(function () {
+    const $modal = $('.js-imgModal');
+    const $modalImg = $('.js-img-insert-target');
+
+    //モーダル開く
+    $(document).on('click', '.js-imgModal-open', function () {
+        const $targetBtn = $(this);
+
+        imgPrev($targetBtn);
+        $modal.fadeIn();
+        $('body').css('overflow', 'hidden');
+        return false;
+    });
+
+    //キャンセルして閉じる
+    $(document).on('click', '.js-imgModal-close', function () {
+        $modal.fadeOut();
+        $('body').css('overflow', 'visible');
+        return false;
+    });
+
+    //削除する
+    $(document).on('click', '.js-imgModal-delete', function () {
+        const dataModal = $modalImg.attr('data-modal');
+        const $deleteTarget = $('.js-prev__img[data-modal="' + dataModal + '"]');
+        const $deleteBtn = $deleteTarget.parents('.js-image-parents').find('.js-imgModal-open')
+
+        $deleteTarget.prop('src', '');
+
+        $modal.fadeOut();
+        $('body').css('overflow', 'visible');
+
+        $deleteBtn.hide();
+        return false;
+    });
+    
+    //モーダルへ画像表示する関数
+    function imgPrev(option) {
+        const $targetBtn = option;
+        
+        const $img = $targetBtn.parents('.js-image-parents').find('.js-prev__img');
+        const src = $img.attr('src');
+        const dataModal = $img.attr('data-modal');
+
+        $modalImg.attr('src',src);
+        $modalImg.attr('data-modal',dataModal);
+    }
+})
--- resources/js/components/previewImage.js
+++ resources/js/components/previewImage.js
@@ -17,21 +17,37 @@ $dropArea.on('dragleave', function(e){
 $productFileInput.on('change', function () {
   $dropArea.css('border', 'none');
   let file = this.files[0],         // 2 file配列にファイルが入ってます
-      $img = $(this).siblings('.js-prev__img'), // 3 jQueryのsiblingsメソッドで兄弟のimg取得
-      fileReader = new FileReader();  // 4 ファイルを読み込むFileReaderオブジェクト
+  $img = $(this).siblings('.js-prev__img'), // 3 jQueryのsiblingsメソッドで兄弟のimg取得
+  fileReader = new FileReader();  // 4 ファイルを読み込むFileReaderオブジェクト
+
+  //5. 読み込みが完了した際のイベントハンドラ。imgのsrcにデータをセット
+  fileReader.onload = function(event) {
+    //読み込んだデータをimgに設定
+    $img.attr('src', event.target.result).show();
+  };
   
-      //5. 読み込みが完了した際のイベントハンドラ。imgのsrcにデータをセット
-      fileReader.onload = function(event) {
-        //読み込んだデータをimgに設定
-        $img.attr('src', event.target.result).show();
-      };
-      
-      // 6. 画像読み込み
-      fileReader.readAsDataURL(file);
-    });
-    
-    
-    
+  // 6. 画像読み込み
+  fileReader.readAsDataURL(file);
+
+  // 削除ボタン表示
+  deletebtnshow();
+});
+
+//削除ボタン表示関数
+let deletebtnshow = function () {
+  let $imgs = $('.js-prev__img');
+
+  for (let i = 0; i < $imgs.length; i++){
+    if ($($imgs[i]).attr('src') !== '/storage/') {
+      $($imgs[i]).parents('.js-image-parents').find('.js-imgModal-open').show();
+    } else {
+      $($imgs[i]).parents('.js-image-parents').find('.js-imgModal-open').hide();
+    }
+  }
+}
+//読み込み時に削除ボタンを表示される
+deletebtnshow();
+
 let $profileFileInput = $('.js-input__file--profile');
 
 //プロフィール画像プレビュー
