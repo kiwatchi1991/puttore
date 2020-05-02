e65ca586f4b9ca4b46c6fed8076dc024ad43565e
脇田　慎平 <ymnkknt3@gmail.com>
2020-05-02 16:15:58 +0900

    メールリセットメール スタイル調整

--- resources/views/emails/changeEmail.blade.php
+++ resources/views/emails/changeEmail.blade.php
@@ -1,11 +1,30 @@
-<a href="{{ config('app.url') }}">ぷっとれ</a>
-<p>
-    下記のURLをクリックして新しいメールアドレスを確定してください。<br>
-</p>
-<p>
-    {{ $actionText }}: <a href="{{ $actionUrl }}">{{ $actionUrl }}</a>
-</p>
+<!DOCTYPE html>
+<html lang="en">
 
-<p>
-    ※URLの有効期限は一時間以内です。有効期限が切れた場合は、お手数ですがもう一度最初からお手続きを行ってください。<br>
-</p>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<body>
+    <style>
+        .c-changeEmail {
+            padding: 16px;
+        }
+    </style>
+    <div class="c-changeEmail">
+        <a href="{{ config('app.url') }}">ぷっとれ</a>
+        <p>
+            下記のURLをクリックして新しいメールアドレスを確定してください。<br>
+        </p>
+        <p>
+            {{ $actionText }}: <a href="{{ $actionUrl }}">{{ $actionUrl }}</a>
+        </p>
+
+        <p>
+            ※URLの有効期限は一時間以内です。有効期限が切れた場合は、お手数ですがもう一度最初からお手続きを行ってください。<br>
+        </p>
+    </div>
+</body>
+
+</html>
