3e23558af3f8725cf5e950ab136ad149162978ec
脇田　慎平 <ymnkknt3@gmail.com>
2020-08-23 22:40:05 +0900

    銀行情報登録画面見た目修正

--- resources/views/mypage/edit.blade.php
+++ resources/views/mypage/edit.blade.php
@@ -16,9 +16,10 @@
             </div>
 
             <div class="c-mypage__account__list">
-                <p class="c-mypage__account__label">銀行コード <span class="c-mypage__editLink"></span></p>
+                <p class="c-mypage__account__label">銀行コード<span class="required">必須</span></p>
                 <input id="bank_code" type="num" class="c-mypage__input-area @error('bank_code')is-invalid @enderror"
-                    name="bank_code" value="{{ $bank->bank_code }}" autocomplete="bank_code" placeholder="" required>
+                    name="bank_code" value="{{ $bank->bank_code }}" autocomplete="bank_code"
+                    placeholder="4桁のコードを入力してください" required>
 
                 @error('bank_code')
                 <span class="c-mypage__error" role="alert">
@@ -28,10 +29,11 @@
             </div>
 
             <div class="c-mypage__account__list">
-                <p class="c-mypage__account__label">支店コード <span class="c-mypage__editLink"></span></p>
+                <p class="c-mypage__account__label">支店コード<span class="required">必須</span></p>
                 <input id="bank_branch_code" type="num"
                     class="c-mypage__input-area @error('bank_branch_code')is-invalid @enderror" name="bank_branch_code"
-                    value="{{ $bank->bank_branch_code }}" autocomplete="bank_branch_code" placeholder="" required>
+                    value="{{ $bank->bank_branch_code }}" autocomplete="bank_branch_code" placeholder="3桁のコードを入力してください"
+                    required>
 
                 @error('bank_branch_code')
                 <span class="c-mypage__error" role="alert">
@@ -40,7 +42,7 @@
                 @enderror
             </div>
             <div class="c-mypage__account__list">
-                <p class="c-mypage__account__label">口座名義人（カタカナ） <span class="c-mypage__editLink"></span></p>
+                <p class="c-mypage__account__label">口座名義人（カタカナ）<span class="required">必須</span></p>
                 <input id="bank_account_holder_name" type="text"
                     class="c-mypage__input-area @error('bank_account_holder_name')is-invalid @enderror"
                     name="bank_account_holder_name" value="{{ $bank->bank_account_holder_name }}"
@@ -53,16 +55,20 @@
                 @enderror
             </div>
             <div class="c-mypage__account__list">
-                <p class="c-mypage__account__label">口座種別 <span class="c-mypage__editLink"></span></p>
-                <select name="bank_account_type" id="bank_account_type">
-                    <option value="">選択してください</option>
-                    <option value="0" selected>普通</option>
-                    <option value="1">当座</option>
-                </select>
-                {{-- <input id="bank_account_type" type="number"
-                    class="c-mypage__input-area @error('bank_account_type')is-invalid @enderror" name="bank_account_type"
-                    value="{{ $bank->bank_account_type }}" autocomplete="bank_account_type"
-                placeholder="" required> --}}
+                <p class="c-mypage__account__label">口座種別<span class="required">必須</span></p>
+                <div class="c-mypage__accountType">
+                    <input type="radio" class="c-mypage__radio" name="bank_account_type" id="bank_account_type[0]"
+                        value="0" checked="{{ $bank->bank_account_type === '0' }}">
+                    <label for="bank_account_type[0]" class="c-mypage__radioLabel">
+                        普通
+                    </label>
+                    <input type="radio" class="c-mypage__radio" name="bank_account_type" id="bank_account_type[1]"
+                        value="1" checked="{{ $bank->bank_account_type === '1' }}">
+                    <label for="bank_account_type[1]" class="c-mypage__radioLabel">
+                        当座
+                    </label>
+                </div>
+
                 @error('bank_account_type')
                 <span class="c-mypage__error" role="alert">
                     <strong>{{ $message }}</strong>
@@ -70,7 +76,7 @@
                 @enderror
             </div>
             <div class="c-mypage__account__list">
-                <p class="c-mypage__account__label">口座番号 <span class="c-mypage__editLink"></span></p>
+                <p class="c-mypage__account__label">口座番号<span class="required">必須</span></p>
                 <input id="bank_account_number" type="number"
                     class="c-mypage__input-area @error('bank_account_number')is-invalid @enderror"
                     name="bank_account_number" value="{{ $bank->bank_account_number }}"
@@ -81,6 +87,11 @@
                     <strong>{{ $message }}</strong>
                 </span>
                 @enderror
+            </div>
+            <div class="c-mypage__account__list">
+                <p class="c-mypage__agree">「銀行情報を登録する」ボタンを押すことにより、<a class="c-mypage__agree__link"
+                        href="https://pay.jp/legal/tos-payouts" target="_blank">PAY.JP
+                        Platform（Payouts）利用規約</a> に同意したものとします。</p>
             </div>
 
             <div class="c-mypage__submit">
