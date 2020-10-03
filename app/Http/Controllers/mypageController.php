5825f0e688b9c2823b9fa9f4bce63c1bdbfbbf1e
脇田　慎平 <ymnkknt3@gmail.com>
2020-10-03 15:51:57 +0900

    入金履歴の表示と、未振込売上のロジック修正

--- app/Http/Controllers/mypageController.php
+++ app/Http/Controllers/mypageController.php
@@ -17,284 +17,268 @@ use Illuminate\Support\Facades\Log;
 
 class mypageController extends Controller
 {
-    public function index(Request $request)
-    {
-        $user = Auth::user();
-        return view('mypage.index', compact('user'));
+  public function index(Request $request)
+  {
+    $user = Auth::user();
+    return view('mypage.index', compact('user'));
+  }
+
+  /**
+   * プロダクト一覧機能
+   */
+
+  public function products(Request $request)
+  {
+    // 下書き保存中の作品数
+    $drafts = Auth::user()->products()
+      ->where('open_flg', 1)
+      ->get();
+
+    //出品作品
+    $products = Auth::user()->products()->latest()->get();
+    $product_category = Product::all();
+    $product_difficulty = Product::all();
+
+    //購入済み作品
+    $buy_products = DB::table('orders')
+      ->where('orders.user_id', Auth::user()->id)
+      ->join('products', 'orders.product_id', '=', 'products.id')
+      ->get();
+
+    return view('mypage.product', [
+      'products' => $products,
+      'product_categories' => $product_category,
+      'product_difficulties' => $product_difficulty,
+      'drafts' => $drafts,
+      'buy_products' => $buy_products,
+    ]);
+  }
+
+  //=============================================
+  //==========        販売履歴ページ     ==========
+  //=============================================
+  public function sold(Request $request)
+  {
+    //売上履歴
+    $sold_data = Order::query()
+      ->join('products', 'orders.product_id', '=', 'products.id')
+      ->where('products.user_id', Auth::user()->id)
+      ->join('users', 'orders.user_id', '=', 'users.id')
+      ->select('orders.id', 'orders.created_at as created_at', 'sale_price', 'products.id as p_id', 'products.name', 'users.id as u_id', 'users.account_name', 'users.pic')
+      ->orderBy('created_at', 'desc')
+      ->paginate(10);
+
+    return view('mypage.sold', [
+      'sold_data' => $sold_data,
+    ]);
+  }
+
+  //=============================================
+  //==========       下書き　ページ     ==========
+  //=============================================
+
+  public function draft(Request $request)
+  {
+
+    $products = Product::where('user_id', Auth::user()->id)
+      ->where('open_flg', 1)
+      ->latest()->get();
+
+    Log::debug(' <<<<<<   マイページ（　下書き　）   >>>>>>>');
+
+    $product_category = Product::all();
+    $product_difficulty = Product::all();
+
+    return view('mypage.draft', [
+      'products' => $products,
+      'product_categories' => $product_category,
+      'product_difficulties' => $product_difficulty,
+    ]);
+  }
+  //=============================================
+  //==========       お気に入り　ページ     ==========
+  //=============================================
+  public function like()
+  {
+    $likeIds = Like::where('likes.user_id', Auth::user()->id)->get('product_id');
+    $products = Product::whereIn('id', $likeIds)->get();
+
+    Log::debug(' <<<<<<   マイページ（　お気に入り　）   >>>>>>>');
+
+    $product_category = Product::all();
+    $product_difficulty = Product::all();
+
+    return view('mypage.like', [
+      'products' => $products,
+      'product_categories' => $product_category,
+      'product_difficulties' => $product_difficulty,
+    ]);
+  }
+
+  //=============================================
+  //==========       購入作品　ページ     ==========
+  //=============================================
+  public function buy()
+  {
+    $productsIds =  Order::where('orders.user_id', Auth::user()->id)->get('product_id');
+    $products = Product::whereIn('id', $productsIds)->get();
+
+    Log::debug(' <<<<<<   マイページ（ 購入作品　）   >>>>>>>');
+
+    $product_category = Product::all();
+    $product_difficulty = Product::all();
+
+    return view('mypage.buy', [
+      'products' => $products,
+      'product_categories' => $product_category,
+      'product_difficulties' => $product_difficulty,
+    ]);
+  }
+  //=============================================
+  //==========       出品作品　ページ     ==========
+  //=============================================
+  public function sale(Request $request)
+  {
+    $products = Product::where('user_id', Auth::user()->id)
+      ->where('open_flg', 0)
+      ->latest()->get();
+
+    Log::debug(' <<<<<<   マイページ（ 出品作品　）   >>>>>>>');
+
+    $product_category = Product::all();
+    $product_difficulty = Product::all();
+
+    return view('mypage.sale', [
+      'products' => $products,
+      'product_categories' => $product_category,
+      'product_difficulties' => $product_difficulty,
+    ]);
+  }
+
+  //=============================================
+  //=====    （銀行情報）編集　ページ     ==========
+  //=============================================
+  public function edit()
+  {
+    $id = Auth::user()->id;
+    $bank = User::find($id);
+
+    return view('mypage.edit', [
+      'id' => $id,
+      'bank' => $bank,
+    ]);
+  }
+
+  //=============================================
+  //=====    （銀行情報）更新　　　　　     ==========
+  //=============================================
+  public function update(UpdateBankInfoRequest $request, $id)
+  {
+    Log::debug('<<<<   update    >>>>>>');
+    Log::debug('<<<<   request    >>>>>>');
+    Log::debug($request);
+
+    $user = User::find($id);
+    try {
+      $payjp_sk = config('services.payjp.sk_live_p');
+      $tenant_id = $user->payjp_tenant_id;
+
+      \Payjp\Payjp::setApiKey($payjp_sk);
+
+      if (empty($tenant_id)) {
+        // ユーザーのpayjp_tenand_idカラムにデータがない場合、テナント新規作成
+        $tenant = \Payjp\Tenant::create(
+          array(
+            //テナントidは指定せず、自動生成させる
+            "name" => $user->email,
+            "platform_fee_rate" => SystemCommission::find(1)->commission_rate * 100,
+            "minimum_transfer_amount" => 5000,
+            "bank_account_holder_name" => $request->bank_account_holder_name,
+            "bank_code" => $request->bank_code,
+            "bank_branch_code" => $request->bank_branch_code,
+            "bank_account_type" => $request->bank_account_type === '0' ? "普通" : "当座",
+            "bank_account_number" => $request->bank_account_number,
+          )
+        );
+      } else {
+        // ユーザーのpayjp_tenand_idカラムにデータが存在する場合は、テナント情報の更新
+        $tenant = \Payjp\Tenant::retrieve($tenant_id);
+
+        $tenant->bank_account_holder_name = $request->bank_account_holder_name;
+        $tenant->bank_code = $request->bank_code;
+        $tenant->bank_branch_code = $request->bank_branch_code;
+        $tenant->bank_account_type = $request->bank_account_type === '0' ? "普通" : "当座";
+        $tenant->bank_account_number = $request->bank_account_number;
+        $tenant->save();
+      }
+    } catch (\Payjp\Error\Card $e) {
+      //
+      return redirect()->route('mypage')->with('flash_message', 'テナント情報の登録に失敗しました。');
+    }
+    $user->payjp_tenant_id = $tenant->id;
+    $user->fill($request->all())->save();
+
+    return redirect()->route('mypage')->with('flash_message', '口座情報を変更しました');
+  }
+
+  /**
+   * 注文管理機能
+   */
+
+  public function order()
+  {
+    $user = Auth::user();
+    $tenant_id = $user->payjp_tenant_id;
+
+    //テナントidが存在しない場合は、アカウント設定ページへリダイレクトさせる
+    if (empty($tenant_id)) {
+      return redirect()->route('mypage')->with('flash_message', 'アカウント設定ページより、銀行情報を登録してください');
+    }
+    //========  今月の売上 ==============
+    $payjp_sk = config('services.payjp.sk_live_p');
+    \Payjp\Payjp::setApiKey($payjp_sk);
+
+    $this_month_charges = \Payjp\Charge::all(array(
+      "tenant" => $tenant_id,
+      "since" => strtotime(Carbon::now()->startOfMonth()) //月初以降
+    ))["data"];
+
+    $this_month_amount =
+      array_sum(
+        array_column(
+          array_filter($this_month_charges, function ($arr) {
+            // 返金済みのもの（refunded === true）は除外する
+            return $arr->refunded === false;
+          }),
+          "amount" //array_columnの第二引数
+        )
+      );
+
+
+    //=========   未振込売上履歴  ==============
+    $all_transfer = (array)\Payjp\TenantTransfer::all(array(
+      "tenant" => $tenant_id,
+    ))["data"];
+    $filter = array_filter($all_transfer, function ($arr) {
+      return $arr->status !== "paid";
+    });
+    if (!empty($filter)) {
+      $untransferred_sale = $filter[0];
+    } else {
+      $untransferred_sale = null;
     }
 
-    /**
-     * プロダクト一覧機能
-     */
+    //==========  振込履歴  ==============
+    $paids = array_filter($all_transfer, function ($arr) {
+      return $arr->status === "paid";
+    });
 
-    public function products(Request $request)
-    {
-        // 下書き保存中の作品数
-        $drafts = Auth::user()->products()
-            ->where('open_flg', 1)
-            ->get();
-
-        //出品作品
-        $products = Auth::user()->products()->latest()->get();
-        $product_category = Product::all();
-        $product_difficulty = Product::all();
-
-        //購入済み作品
-        $buy_products = DB::table('orders')
-            ->where('orders.user_id', Auth::user()->id)
-            ->join('products', 'orders.product_id', '=', 'products.id')
-            ->get();
-
-        return view('mypage.product', [
-            'products' => $products,
-            'product_categories' => $product_category,
-            'product_difficulties' => $product_difficulty,
-            'drafts' => $drafts,
-            'buy_products' => $buy_products,
-        ]);
-    }
-
-    //=============================================
-    //==========        販売履歴ページ     ==========
-    //=============================================
-    public function sold(Request $request)
-    {
-        //売上履歴
-        $sold_data = Order::query()
-            ->join('products', 'orders.product_id', '=', 'products.id')
-            ->where('products.user_id', Auth::user()->id)
-            ->join('users', 'orders.user_id', '=', 'users.id')
-            ->select('orders.id', 'orders.created_at as created_at', 'sale_price', 'products.id as p_id', 'products.name', 'users.id as u_id', 'users.account_name', 'users.pic')
-            ->orderBy('created_at', 'desc')
-            ->paginate(10);
-
-        return view('mypage.sold', [
-            'sold_data' => $sold_data,
-        ]);
-    }
-
-    //=============================================
-    //==========       下書き　ページ     ==========
-    //=============================================
-
-    public function draft(Request $request)
-    {
-
-        $products = Product::where('user_id', Auth::user()->id)
-            ->where('open_flg', 1)
-            ->latest()->get();
-
-        Log::debug(' <<<<<<   マイページ（　下書き　）   >>>>>>>');
-
-        $product_category = Product::all();
-        $product_difficulty = Product::all();
-
-        return view('mypage.draft', [
-            'products' => $products,
-            'product_categories' => $product_category,
-            'product_difficulties' => $product_difficulty,
-        ]);
-    }
-    //=============================================
-    //==========       お気に入り　ページ     ==========
-    //=============================================
-    public function like()
-    {
-        $likeIds = Like::where('likes.user_id', Auth::user()->id)->get('product_id');
-        $products = Product::whereIn('id', $likeIds)->get();
-
-        Log::debug(' <<<<<<   マイページ（　お気に入り　）   >>>>>>>');
-
-        $product_category = Product::all();
-        $product_difficulty = Product::all();
-
-        return view('mypage.like', [
-            'products' => $products,
-            'product_categories' => $product_category,
-            'product_difficulties' => $product_difficulty,
-        ]);
-    }
-
-    //=============================================
-    //==========       購入作品　ページ     ==========
-    //=============================================
-    public function buy()
-    {
-        $productsIds =  Order::where('orders.user_id', Auth::user()->id)->get('product_id');
-        $products = Product::whereIn('id', $productsIds)->get();
-
-        Log::debug(' <<<<<<   マイページ（ 購入作品　）   >>>>>>>');
-
-        $product_category = Product::all();
-        $product_difficulty = Product::all();
-
-        return view('mypage.buy', [
-            'products' => $products,
-            'product_categories' => $product_category,
-            'product_difficulties' => $product_difficulty,
-        ]);
-    }
-    //=============================================
-    //==========       出品作品　ページ     ==========
-    //=============================================
-    public function sale(Request $request)
-    {
-        $products = Product::where('user_id', Auth::user()->id)
-            ->where('open_flg', 0)
-            ->latest()->get();
-
-        Log::debug(' <<<<<<   マイページ（ 出品作品　）   >>>>>>>');
-
-        $product_category = Product::all();
-        $product_difficulty = Product::all();
-
-        return view('mypage.sale', [
-            'products' => $products,
-            'product_categories' => $product_category,
-            'product_difficulties' => $product_difficulty,
-        ]);
-    }
-
-    //=============================================
-    //=====    （銀行情報）編集　ページ     ==========
-    //=============================================
-    public function edit()
-    {
-        $id = Auth::user()->id;
-        $bank = User::find($id);
-
-        return view('mypage.edit', [
-            'id' => $id,
-            'bank' => $bank,
-        ]);
-    }
-
-    //=============================================
-    //=====    （銀行情報）更新　　　　　     ==========
-    //=============================================
-    public function update(UpdateBankInfoRequest $request, $id)
-    {
-        Log::debug('<<<<   update    >>>>>>');
-        Log::debug('<<<<   request    >>>>>>');
-        Log::debug($request);
-
-        $user = User::find($id);
-        try {
-            $payjp_sk = config('services.payjp.sk_live_p');
-            $tenant_id = $user->payjp_tenant_id;
-
-            \Payjp\Payjp::setApiKey($payjp_sk);
-
-            if (empty($tenant_id)) {
-                // ユーザーのpayjp_tenand_idカラムにデータがない場合、テナント新規作成
-                $tenant = \Payjp\Tenant::create(
-                    array(
-                        //テナントidは指定せず、自動生成させる
-                        "name" => $user->email,
-                        "platform_fee_rate" => SystemCommission::find(1)->commission_rate * 100,
-                        "minimum_transfer_amount" => 5000,
-                        "bank_account_holder_name" => $request->bank_account_holder_name,
-                        "bank_code" => $request->bank_code,
-                        "bank_branch_code" => $request->bank_branch_code,
-                        "bank_account_type" => $request->bank_account_type === '0' ? "普通" : "当座",
-                        "bank_account_number" => $request->bank_account_number,
-                    )
-                );
-            } else {
-                // ユーザーのpayjp_tenand_idカラムにデータが存在する場合は、テナント情報の更新
-                $tenant = \Payjp\Tenant::retrieve($tenant_id);
-
-                $tenant->bank_account_holder_name = $request->bank_account_holder_name;
-                $tenant->bank_code = $request->bank_code;
-                $tenant->bank_branch_code = $request->bank_branch_code;
-                $tenant->bank_account_type = $request->bank_account_type === '0' ? "普通" : "当座";
-                $tenant->bank_account_number = $request->bank_account_number;
-                $tenant->save();
-            }
-        } catch (\Payjp\Error\Card $e) {
-            //
-            return redirect()->route('mypage')->with('flash_message', 'テナント情報の登録に失敗しました。');
-        }
-        $user->payjp_tenant_id = $tenant->id;
-        $user->fill($request->all())->save();
-
-        return redirect()->route('mypage')->with('flash_message', '口座情報を変更しました');
-    }
-
-    /**
-     * 注文管理機能
-     */
-
-    public function order()
-    {
-        $user = Auth::user();
-        $tenant_id = $user->payjp_tenant_id;
-
-        //テナントidが存在しない場合は、アカウント設定ページへリダイレクトさせる
-        if (empty($tenant_id)) {
-            return redirect()->route('mypage')->with('flash_message', 'アカウント設定ページより、銀行情報を登録してください');
-        }
-        //========  今月の売上 ==============
-        $payjp_sk = config('services.payjp.sk_live_p');
-        \Payjp\Payjp::setApiKey($payjp_sk);
-
-        $this_month_charges = \Payjp\Charge::all(array(
-            "tenant" => $tenant_id,
-            "since" => strtotime(Carbon::now()->startOfMonth()) //月初以降
-        ))["data"];
-
-        $this_month_amount =
-            array_sum(
-                array_column(
-                    array_filter($this_month_charges, function ($arr) {
-                        // 返金済みのもの（refunded === true）は除外する
-                        return $arr->refunded === false;
-                    }),
-                    "amount" //array_columnの第二引数
-                )
-            );
-
-
-        //=========   未振込売上履歴  ==============
-        $all_transfer = (array)\Payjp\TenantTransfer::all(array(
-            "tenant" => $tenant_id,
-        ))["data"];
-        $filter = array_filter($all_transfer, function ($arr) {
-            return $arr->status !== "paid";
-        });
-        $untransferred_sale = $filter[0];
-
-        //==========  振込履歴  ==============
-        $paids = array_filter($all_transfer, function ($arr) {
-            return $arr->status === "paid";
-        });
-
-        return view('mypage.order', [
-            'this_month_amount' => $this_month_amount,
-            'untransferred_sale' => $untransferred_sale,
-            'user' => $user,
-            'paids' => $paids,
-            'filter' => $filter,
-        ]);
-    }
-
-    public function getSampleData()
-    {
-        $user = Auth::user();
-        $tenant_id = $user->payjp_tenant_id;
-
-        $payjp_sk = config('services.payjp.sk_live_p');
-        \Payjp\Payjp::setApiKey($payjp_sk);
-
-        $all_transfer = (array)\Payjp\TenantTransfer::all(array(
-            // "tenant" => $tenant_id,
-            "tenant" => "ten_e1c93880cbe965d7634427feb032",
-        ))["data"];
-        $filter = array_filter($all_transfer, function ($arr) {
-            return $arr->status !== "paid";
-        });
-
-        // return response()->json($filter[0]->amount);
-        return view('mypage.data');
-    }
+    return view('mypage.order', [
+      'this_month_amount' => $this_month_amount,
+      'untransferred_sale' => $untransferred_sale,
+      'user' => $user,
+      'paids' => $paids,
+      'filter' => $filter,
+    ]);
+  }
 }
--- resources/views/mypage/order.blade.php
+++ resources/views/mypage/order.blade.php
@@ -5,78 +5,89 @@
 @include('mypage.nav', ['page' => 'order'])
 
 <div class="c-mypage__order">
-    <div class="c-mypage__sale">
-        <div class="c-mypage__products__title c-mypage__products__title--sale">
-            <h2>販売管理</h2>
-        </div>
-
-        <div class="c-mypage__sale__thisMonth">
-            <div class="c-mypage__sale__thisMonth__title">
-                <p>今月の売上</p>
-            </div>
-            <div class="c-mypage__sale__thisMonth__price__wrapper">
-
-                <div class="c-mypage__sale__thisMonth__total">総額</div>
-                <div class="c-mypage__sale__thisMonth__price"><span class="c-mypage__sale__icon">¥</span>
-                    {{ number_format($this_month_amount) }}
-                </div>
-                <div style="width:50px;"></div>
-            </div>
-        </div>
-
-        {{-- 次回振込予定 --}}
-        <div class="c-mypage__sale__untransferred">
-            <div class="c-mypage__sale__untransferred__title">
-                <p>次回振込予定金額</p>
-                @if($untransferred_sale->status === "pending")
-                <p class=c-mypage__sale__untransferred__scheduled>振込予定日：{{ $untransferred_sale->scheduled_date }}</p>
-                @endif
-                @if($untransferred_sale->status === ("failed" || "recombination"))
-                <p class=c-mypage__sale__untransferred__failed>振込に失敗しております。大変お手数をお掛けいたしますが、運営にお問い合わせをお願い致します。</p>
-                @endif
-            </div>
-            <div class="c-mypage__sale__untransferred__price__wrapper">
-                <div class="c-mypage__sale__untransferred__total">総額</div>
-
-                <div class="c-mypage__sale__untransferred__price"><span class="c-mypage__sale__icon">¥</span>
-                    {{ number_format($untransferred_sale->amount) }}
-                </div>
-                <div style="width:50px;"></div>
-            </div>
-            <p class="c-mypage__sale__untransferred__text">
-                月末時点での未振込の売上金総額が5,000円以上になると、翌月末に振込されます<br>(月末締め日の翌営業日に最新の情報に更新されます。)。
-            </p>　
-        </div>
-
-        {{-- 振込履歴一覧 --}}
-
-        <div class="c-mypage__sale__done">
-            <div class="c-mypage__sale__done__title">
-                <p>振込履歴</p>
-            </div>
-            <table>
-                <thead>
-                    <tr>
-                        <th class="c-mypage__sale__list c-mypage__sale__list--day">振込日</th>
-                        <th class="c-mypage__sale__list c-mypage__sale__list--price">振込金額</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach ($paids as $paid)
-                    <tr>
-                        <td class="day">{{ $paid->transfer_date && $paid->transfer_date->format('Y年m月d日') }}</td>
-                        <td>¥ {{ number_format($paid->transfer_amount) }}</td>
-                    </tr>
-                    @endforeach
-
-                </tbody>
-            </table>
-            {{-- 金額が0円の場合はDOMが表示されなくなるので、これを表示 --}}
-            @if ($paids == null)
-            <div class="c-mypage__sale__list__text">※振込履歴はありません。</div>
-            @endif
-        </div>
-
+  <div class="c-mypage__sale">
+    <div class="c-mypage__products__title c-mypage__products__title--sale">
+      <h2>販売管理</h2>
     </div>
+
+    <div class="c-mypage__sale__thisMonth">
+      <div class="c-mypage__sale__thisMonth__title">
+        <p>今月の売上</p>
+      </div>
+      <div class="c-mypage__sale__thisMonth__price__wrapper">
+
+        <div class="c-mypage__sale__thisMonth__total">総額</div>
+        <div class="c-mypage__sale__thisMonth__price"><span class="c-mypage__sale__icon">¥</span>
+          {{ number_format($this_month_amount) }}
+        </div>
+        <div style="width:50px;"></div>
+      </div>
+    </div>
+
+    {{-- 次回振込予定 --}}
+    <div class="c-mypage__sale__untransferred">
+      <div class="c-mypage__sale__untransferred__title">
+        <p>次回振込予定金額</p>
+        @if(!empty($untransferred_sale))
+        @if($untransferred_sale->status === "pending")
+        <p class=c-mypage__sale__untransferred__scheduled>振込予定日：{{ $untransferred_sale->scheduled_date }}</p>
+        @endif
+        @if($untransferred_sale->status === ("failed" || "recombination"))
+        <p class=c-mypage__sale__untransferred__failed>振込に失敗しております。大変お手数をお掛けいたしますが、運営にお問い合わせをお願い致します。</p>
+        @endif
+        @endif
+      </div>
+      <div class="c-mypage__sale__untransferred__price__wrapper">
+        <div class="c-mypage__sale__untransferred__total">総額</div>
+
+        <div class="c-mypage__sale__untransferred__price"><span class="c-mypage__sale__icon">¥</span>
+          @if(!empty($untransferred_sale))
+          {{ number_format($untransferred_sale->amount + $untransferred_sale->carried_balance)}}
+          @else
+          0
+          @endif
+        </div>
+        <div style="width:50px;"></div>
+      </div>
+      <p class="c-mypage__sale__untransferred__text">
+        月末時点での未振込の売上金総額が5,000円以上になると、翌月末に振込されます<br>(月末締め日の翌営業日に最新の情報に更新されます。)。
+      </p>　
+    </div>
+
+    {{-- 振込履歴一覧 --}}
+
+    <div class="c-mypage__sale__done">
+      <div class="c-mypage__sale__done__title">
+        <p>振込履歴</p>
+      </div>
+      <table>
+        <thead>
+          <tr>
+            <th class="c-mypage__sale__list c-mypage__sale__list--day">振込日</th>
+            <th class="c-mypage__sale__list c-mypage__sale__list--price">振込金額</th>
+          </tr>
+        </thead>
+        <tbody>
+          @foreach ($paids as $paid)
+          <tr>
+            {{-- 日付を文字列から日付の形式に変換 --}}
+            @php $timestamp = strtotime($paid->transfer_date);
+            $transfer_date = date("Y年m月d日", $timestamp);
+            @endphp
+
+            <td class="day">{{ $transfer_date }}</td>
+            <td>¥ {{ number_format($paid->transfer_amount) }}</td>
+          </tr>
+          @endforeach
+
+        </tbody>
+      </table>
+      {{-- 金額が0円の場合はDOMが表示されなくなるので、これを表示 --}}
+      @if ($paids == null)
+      <div class="c-mypage__sale__list__text">※振込履歴はありません。</div>
+      @endif
+    </div>
+
+  </div>
 </div>
 @endsection
